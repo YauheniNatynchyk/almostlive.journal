d5f20e2852175a88350b2bfeabe7b65ae4ea9415
Eugen <konorid3@gmail.com>
2024-08-10 14:50:07 +0200

    JS update

---
+++ docs/script.js
@@ -0,0 +1,26 @@
+function convertToNumber(value) {
+    // Convert a string value to a number if possible
+    let number_value = Number(value);
+    if (Number.isNaN(number_value)) {
+      return 0
+    } else {
+      return number_value
+    }
+  }
+  
+  
+  
+  document.getElementById('addcomm').addEventListener('click', (event) => {
+    let element_comment_section = document.getElementById('comment_section');
+    let new_span = document.createElement('span');
+    new_span.innerText = document.getElementById('username').value;
+    new_span.classList.toggle('content');
+  
+    element_comment_section.appendChild(new_span);
+    let new_li = document.createElement('li');
+    new_li.innerText = document.getElementById('commentinp').value;
+    new_li.classList.toggle('content');
+  
+    element_comment_section.appendChild(new_li);
+  
+  });
