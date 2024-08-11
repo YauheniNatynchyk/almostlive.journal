5477ce0552241ffbf2124392ea1bb098b2ea33cf
konorid <konorid3@gmail.com>
2024-08-11 20:49:57 +0200

    making carousel

--- carousel.js
+++ carousel.js
@@ -1,17 +1,10 @@
 var pics;
 
 
-pics = ['https://i.imgur.com/sAIsS32.jpeg', 
-    'https://i.imgur.com/JYt01aR.jpeg',
-    'https://i.imgur.com/x5PyelT.jpeg',
-    'https://i.imgur.com/OAUoRU7.jpeg',
-    'https://i.imgur.com/wWwXQ8H.jpeg',
-    'https://i.imgur.com/mKB9GKJ.jpeg',
-    'https://i.imgur.com/dXt694V.jpeg',
-    'https://i.imgur.com/3n2AXop.jpeg'];
+pics = ['https://i.ibb.co/VJ3xjhS/1-46.jpg', 'https://i.ibb.co/w0Jzqcr/1-184.jpg', 'https://i.ibb.co/SyG9DN7/1-303.jpg', 'https://i.ibb.co/ckcpTQJ/1-322.jpg', 'https://i.ibb.co/QKG7xsM/1-355.jpg', 'https://i.ibb.co/W51dqv2/1-353.jpg', 'https://i.ibb.co/F7VFRzv/DSC00605.jpg', 'https://i.ibb.co/zXYTPQV/DSC00667.jpg'];
 
 let element_pic = document.getElementById('pic');
-element_pic.setAttribute("src", 'https://i.imgur.com/QkUK7JO.jpeg');
+element_pic.setAttribute("src", 'https://i.ibb.co/fdYgj7q/DSChttps://i.ibb.co/fdYgj7q/DSC00511.jpg00511.jpg');
 
 
 document.getElementById('next').addEventListener('click', (event) => {
