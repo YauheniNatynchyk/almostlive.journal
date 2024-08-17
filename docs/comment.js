980bf558aa5b0f7edb080c4676b4a6c51b0e11be
konorid <konorid3@gmail.com>
2024-08-17 09:54:39 +0200

    Update comment.js

--- docs/comment.js
+++ docs/comment.js
@@ -2,28 +2,27 @@ function convertToNumber(value) {
   // Convert a string value to a number if possible
   let number_value = Number(value);
   if (Number.isNaN(number_value)) {
-    return 0
+  return 0
   } else {
-    return number_value
+  return number_value
   }
-}
-
-
-
-document.getElementById('addcomm').addEventListener('click', (event) => {
+  }
+  
+  document.getElementById('addcomm').addEventListener('click', (event) => {
   let element_comment_section = document.getElementById('comment_section');
   if (!(document.getElementById('username').value == '')) {
-    let new_span = document.createElement('span');
-    new_span.innerText = document.getElementById('username').value;
-    new_span.classList.toggle('header');
-
-    element_comment_section.appendChild(new_span);
-    let new_li = document.createElement('li');
-    new_li.setAttribute("class", 'comment');
-    new_li.innerText = document.getElementById('commentinp').value;
-
-    element_comment_section.appendChild(new_li);
+  let new_span = document.createElement('span');
+  new_span.setAttribute("class", 'article a');
+  new_span.innerText = document.getElementById('username').value;
+  
+ 
+  element_comment_section.appendChild(new_span);
+  let new_li = document.createElement('li');
+  new_li.setAttribute("class", 'comment');
+  new_li.innerText = document.getElementById('commentinp').value;
+  
+  element_comment_section.appendChild(new_li);
+    
   }
-
-});
-
+  
+  });
